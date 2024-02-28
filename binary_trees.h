b3a6f7c2f0415045a9b154bff4b2ce17405f5060
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-28 04:47:44 -0800

    header updated

--- binary_trees.h
+++ binary_trees.h
@@ -1,9 +1,11 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
+#define max(a, b) ((a) > (b) ? (a): (b))
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /**
  * struct binary_tree_s - Binary tree node
