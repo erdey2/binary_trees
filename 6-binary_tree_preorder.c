0fc0d008ddde7c8fb936ecf969e4493bc238b173
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-28 02:23:02 -0800

    print nodes in preorder

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - print nodes in preorder(parent->left->right)
+ * @tree: the address of the root node
+ *
+ * Return: void
+  */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL)
+	{
+		printf("%d\n", tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
