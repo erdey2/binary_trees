3ae204c79ff792ae59bf152012dbe4cc253d8c48
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-28 05:26:05 -0800

    size of binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -13,7 +13,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	}
 	else
 	{
-		return (1 + max(binary_tree_size(tree->left),
-					binary_tree_size(tree->right)));
+		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 	}
 }
