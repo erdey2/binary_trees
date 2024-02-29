3aa0d573a35fd0cc6e2ab92518436b6cab24b1cb
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-29 03:40:41 -0800

    delete all the nodes in binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - delete all the nodes in binary tree
+ * @tree: address of the first node
+ *
+ * Return: void
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}	
