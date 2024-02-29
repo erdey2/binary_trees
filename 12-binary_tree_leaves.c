69ab9f42ca466e46fed0fc2d364a88b6395cc8e0
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-28 23:50:55 -0800

    count the leave nodes

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,18 +2,19 @@
 /**
  * binary_tree_leaves - count leaves
  * @tree: address of the first node
- * 
+ *
  * Return: the total leaves
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t total = 0;
-
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL || tree->right == NULL)
+	else if (tree->left == NULL && tree->right == NULL)
 	{
-		total += 1;
+		return (1);
 	}
-	return (total);
+	else
+	{
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	}
 }
