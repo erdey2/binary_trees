bc92eb0446cedd753a70408b223cc5048a59119a
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-29 00:46:18 -0800

    count nodes with at least one child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	}
 	if (tree->left != NULL || tree->right != NULL)
 	{
-		return (1 + (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right)));
+		return (1 + (binary_tree_nodes(tree->left) +
+					binary_tree_nodes(tree->right)));
 	}
 	else
 		return (0);
